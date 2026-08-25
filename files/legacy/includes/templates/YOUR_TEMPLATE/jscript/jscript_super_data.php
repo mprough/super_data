0b9e1bf62f109a05803912b21b2bc78be00dd491
Melanie Prough <mprough@pro-webs.net>
2026-08-25 06:28:28 -0400

    Correct legacy customer-paid return shipping markup

--- files/legacy/includes/templates/YOUR_TEMPLATE/jscript/jscript_super_data.php
+++ files/legacy/includes/templates/YOUR_TEMPLATE/jscript/jscript_super_data.php
@@ -984,7 +984,7 @@ if ($applicableReturnCountry !== '' && isset($returnPolicyCategory[PLUGIN_SUPERD
         && $rType === 'ReturnShippingFees'
         && $rFeeNumeric !== ''
         && is_numeric($rFeeNumeric)
-        && (float)$rFeeNumeric >= 0) {
+        && (float)$rFeeNumeric > 0) {
         $policyData['returnShippingFeesAmount'] = [
             '@type' => 'MonetaryAmount',
             'currency' => $rCurrency,
